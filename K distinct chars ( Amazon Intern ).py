a4512fc3379a0491aca383023fcaee64fc49817e
VS Chandra Mourya <30852815+VSMourya@users.noreply.github.com>
2019-11-16 20:04:52 +0530

    Add files via upload

---
+++ K distinct chars ( Amazon Intern ).py
@@ -0,0 +1,21 @@
+#
+# Given a string s and an int k, return an int representing the number of substrings (not unique) of s
+# with exactly k distinct characters. If the given string doesn't have k distinct characters, return 0.
+
+def answer(s,k):
+
+    ls = []
+
+    for i in range(0,len(s)):
+        for j in range(i+1,len(s)):
+            if len(set(s[i:j+1])) == k:
+                ls.append(s[i:j+1])
+
+    print(ls)
+    return len(ls)
+
+
+s = "pqpqs"
+k = 2
+
+print(answer(s,k))
